c092394966cb9c1df03d2c5df62c0775b8f9b590
OlenaTS <tsynda.elena@gmail.com>
2019-02-17 12:53:05 +0200

    My first commit.

---
+++ src/Lesson1HomeTask.java
@@ -0,0 +1,13 @@
+public class Lesson1HomeTask {
+    public static void main(String[] args) {
+
+        System.out.println("****  *****     *      *      *");
+        System.out.println("*     *   *    *  *    * *  * *");
+        System.out.println("***   *****   *    *   *   *  *");
+        System.out.println("*     *      ********  *      *");
+        System.out.println("****  *     *        * *      *");
+
+
+
+    }
+}
